fa7bda426d4f2c02b55207b601261809a590526c
Mathieu Piot <mathieu.piot@inra.fr>
2017-09-08 09:10:09 +0200

    Fix cache owner when clear cache in app entrypoint

--- docker/app/docker-entrypoint.sh
+++ docker/app/docker-entrypoint.sh
@@ -4,4 +4,7 @@
 /var/www/html/bin/console cache:clear --no-warmup --env ${SYMFONY_ENV}
 /var/www/html/bin/console cache:warmup --env ${SYMFONY_ENV}
 
+# Re-define the user
+chown -R www-data:www-data /var/www/html/var/cache/${SYMFONY_ENV}
+
 exec "$@"
