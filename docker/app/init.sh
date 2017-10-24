ff12cab52d216e54eb991db9cf01a4b65be5a01a
Mathieu Piot <mathieu.piot@inra.fr>
2017-10-24 15:01:22 +0200

    App: Fix the init.sh script and reorganise Dockerfile to avoid multiple intermediate images

--- docker/app/init.sh
+++ docker/app/init.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
 # Clear the cache and warm it
-/var/www/html/bin/console cache:clear --no-warmup --env ${SYMFONY_ENV}
-/var/www/html/bin/console cache:warmup --env ${SYMFONY_ENV}
+/var/www/html/bin/console cache:clear --no-warmup
+/var/www/html/bin/console cache:warmup
 
-# Add a sleep, because sessions folder is created 10-20 seconds after php-fpm init
-sleep 10
-
-# Re-define the user
+# Because the script was executed as root, re-define the user
 chown -R www-data:www-data /var/www/html/var
